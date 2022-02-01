5292bf27e955454c39849911b37dd90dd3d86327
Erin Szabo <eszabo@cocc.edu>
2022-02-01 13:50:53 -0800

    Test user attributes.
    Reference #18

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  
+  describe "attributes" do
+    fake_email = "fake@fake.com"
+    fake_pass = "pass1234"
+    it "is valid with valid attributes" do
+      expect(User.new(email:fake_email, password:fake_pass)).to be_valid
+    end
+    it "is not valid without an email" do
+      expect(User.new(password:fake_pass)).to be_invalid
+    end
+    it "is not valid without a password" do
+      expect(User.new(email:fake_email)).to be_invalid
+    end
+  end
 end
