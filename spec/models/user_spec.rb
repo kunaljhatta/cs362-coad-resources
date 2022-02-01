aa7b70247df4c55af508c895bb2275d0f529cc94
Erin Szabo <eszabo@cocc.edu>
2022-02-01 15:09:53 -0800

    Setup what needs to be validated.
    Reference #20

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+
   describe "attributes" do
     fake_email = "fake@fake.com"
     fake_pass = "pass1234"
@@ -14,4 +15,12 @@ RSpec.describe User, type: :model do
       expect(User.new(email:fake_email)).to be_invalid
     end
   end
+
+  describe "validations" do
+    it "validates length of email"
+    it "validates email format"
+    it "validates uniqueness of email"
+    it "validates length of password"
+  end
+
 end
