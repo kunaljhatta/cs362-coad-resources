3f019f3c7491c0a915fdd21eb64b50d1e6c801ba
Erin Szabo <eszabo@cocc.edu>
2022-02-08 13:57:19 -0800

    Fix indentation in users factory.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
-    factory :user do
-        email { "factory_user@fake.fake" }
-        password { "factory_password" }
-    end
+  factory :user do
+    email { "factory_user@fake.fake" }
+    password { "factory_password" }
+  end
 end
