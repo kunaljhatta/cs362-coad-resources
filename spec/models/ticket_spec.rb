25849a4d468c7fd5d60a15c702541e931d718547
Erin Szabo <eszabo@cocc.edu>
2022-02-08 15:06:47 -0800

    Setup attributes to be tested.
    Reference #10

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -2,23 +2,24 @@ require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
 
-  let(:ticket) { build(:ticket) } 
+  #let(:ticket) { build(:ticket) } 
 
-  describe "attributes" do
-
-    it "exists" do
-      ticket
-    end    
+  describe "attributes" do 
 
     it "is valid with valid attributes" do
       name = "fake name"
       phone = "1231234567"
-      region_id = Region.create
-      resource_category_id = ResourceCategory.create(name: name)
-      expect(Ticket.new(name: name, phone: phone, region_id: region_id, resource_category_id: resource_category_id))
-      
-    #  expect(ticket).to be_valid
+      region_id = Region.create(name: "Bend")
+      resource_category_id = ResourceCategory.create(name: "Fake Name")
+      ticket = Ticket.create(name: name, phone: phone, region_id: region_id, resource_category_id: resource_category_id)
+
+      # expect(ticket).to be_valid
     end
+    
+    it "is not valid without a name"
+    it "is not valid without a phone number"
+    it "is not valid without a region id"
+    it "is not valid without a resource category id"
   
   end
 
