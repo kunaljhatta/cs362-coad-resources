5500b8159463d5caaf3085372084cd35250b3438
KunalJhatta <Kunaljhatta22@gmail.com>
2022-02-08 19:32:22 -0800

    changed the region model with shoulda-matchers and fixed some of the issues References #12

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -1,72 +1,59 @@
 require 'rails_helper'
 
 RSpec.describe Region, type: :model do
-    let(:rg) { build(:region) }
 
-    let(:region){ Region.new(name: 'Fake Region')}
-        it 'exists' do
-            Region
-        end
+  let(:region)  {Region.new(
+    name: "Region"
+  )}
+  
+  describe "attributes" do 
+    it "has a name" do 
+      region = Region.new
+      expect(region).to respond_to(:name)
+    end
+  end
 
-    describe "attributes" do 
-        it "has a name" do
-            region = Region.new
-            expect(region).to respond_to(:name)
-        end
+  describe "validators" do
+    it "validates presence of name" do
+      should validate_presence_of(:name)
     end
 
-    describe "#to_s" do
-        it "returns the name" do
-            name = 'Mt. Hood'
-            region = Region.new(name: name)
-            result = region.to_s
-            expect(result).to eq(name) 
-        end
+    it "validates length of name" do
+      should validate_length_of(:name).
+        is_at_least(1).is_at_most(255)
     end
 
-    describe "::unspecified" do
-        it "returns a Region with the name 'Unspecified'" do
-            region = Region.unspecified
-            expect(region.name).to eq("Unspecified")
-        end
+    it "validates uniqueness of name" do
+      should validate_uniqueness_of(:name).case_insensitive
+    end
+  end
+
+  describe "association" do
+    it "Has many tickets" do
+      should have_many :tickets
+    end
+  end
+
+  describe "validators" do
+    it "validates the presence of name" do
+      should validate_presence_of(:name)
     end
 
-    describe "associations" do
-        it "has many tickets" do 
-            expect(Region.new).to have_many(:tickets)
-        end 
+    it "validates the length of name" do
+      should validate_length_of(:name).is_at_least(1).is_at_most(255)
     end
-    
-    describe "attributes" do
-        it "validates presence of name" do
-          expect(region).to validate_presence_of(:name)
-        end
 
-        it "name is not blank" do
-            region = Region.new(name: nil)
-            expect(region).to be_invalid
-        end
-    
-        it "validates name length minimum" do
-          expect(region).to validate_length_of(:name).is_at_least(1)
-        end
+    it "validates uniqueness of name" do
+      should validate_uniqueness_of(:name).ignoring_case_sensitivity
+    end
+  end
 
-        it "validates name length maximum" do
-            expect(region).to validate_length_of(:name).is_at_most(255)
-          end
-        
-        it "validates name uniqueness" do
-          expect(region).to validate_uniqueness_of(:name).case_insensitive
-        end
+  describe "#to_s" do
+    it "returns the name" do
+      name = 'Mt. Hood'
+      region = Region.new(name: name)
+      expect(region.to_s).to eq(name)
+    end
+  end
 
-        it "expects name to be unique" do
-            name = "Unique Region name"
-            reqion = Region.create(name: name)
-            duplicate_region = Region.new(name: name)
-            expect(region).to be_valid
-            expect(duplicate_region).to be_invalid
-        end 
-      end
-      
 end
-
