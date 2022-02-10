29011133af17fc642de76f33ad71db873c74d731
Paul Koos <paully_k5298@yahoo.com>
2022-02-10 11:22:15 -0800

    add another resource category factory for inactive. Refactor the remaining tests to use factories.
    References #36.

--- spec/factories/resource_categories.rb
+++ spec/factories/resource_categories.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
+  
   factory :resource_category do
+    
     name { "Fake Category" }
+    
+    factory :inactive do
+      active { false }
+    end
+    
   end
+
 end
--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -1,96 +1,83 @@
 require 'rails_helper'
 
 RSpec.describe ResourceCategory, type: :model do
-  let(:build_category) { FactoryBot.build(:resource_category) }
-  let(:stubbed_category) {FactoryBot.build_stubbed(:resource_category) }
+  let(:build_cat) { FactoryBot.build(:resource_category) }
+  let(:stubbed_cat) {FactoryBot.build_stubbed(:resource_category) }
   let(:create_cat) { create(:resource_category) }
+  let(:unspecified) { ResourceCategory.unspecified }
+  let(:inactive) { FactoryBot.build(:inactive)}
 
   describe "attributes" do
     it "resource category is valid when name is not blank" do
-      expect(build_category).to be_valid
+      expect(build_cat).to be_valid
     end
 
     it "resource category is not valid with blank name" do
-      build_category.name = nil
-      expect(build_category).not_to be_valid
+      build_cat.name = nil
+      expect(build_cat).not_to be_valid
     end
 
     it "category requires name is unique" do
       category_name = create_cat.name
-      expect(build_category).to be_invalid
+      expect(build_cat).to be_invalid
     end  
-  end
-
-
-  describe "instantiation" do
-    it "exists" do
-      ResourceCategory
-    end
   end
 
   describe "associations" do
     it "has and belongs to many :organizations" do
-      resource_category = ResourceCategory.new
-      expect { resource_category.should have_and_belong_to_many(:organizations) }
+      
+      expect { build_cat.should have_and_belong_to_many(:organizations) }
     end
 
     it "has many tickets" do
-      resource_category = ResourceCategory.new
-      expect { resource_category.should have_many(:tickets) }
+      expect { build_cat.should have_many(:tickets) }
     end
 
   end
 
   describe "validations" do
-    resource_category = ResourceCategory.new
-    it { resource_category.should validate_presence_of(:name) }
-    it { resource_category.should validate_length_of(:name) }
-    it { resource_category.should validate_uniqueness_of(:name).case_insensitive }
+    it { build_cat.should validate_presence_of(:name) }
+    it { build_cat.should validate_length_of(:name) }
+    it { build_cat.should validate_uniqueness_of(:name).case_insensitive }
   end
 
 
   describe "method" do
     it "using unspecified will return category with name 'Unspecified'" do
-      resource_category = ResourceCategory.unspecified
-      expect(resource_category.name).to eq("Unspecified")
+      expect(unspecified.name).to eq("Unspecified")
     end
 
     it "using activate method will set active to true" do
-      resource_category = ResourceCategory.new(active:false)
-      resource_category.activate
-      expect(resource_category.active).to be_truthy
+      inactive.activate
+      expect(inactive.active).to be_truthy
     end
 
     it "using deactivate will set active to false" do
-      resource_category = ResourceCategory.new
-      resource_category.deactivate
-      expect(resource_category.active).to be_falsy
+      build_cat.deactivate
+      expect(build_cat.active).to be_falsy
     end
 
     it "inactive? returns true when active is false" do
-      resource_category = ResourceCategory.new
-      expect(resource_category.inactive?).not_to be_truthy
+      expect(build_cat.inactive?).not_to be_truthy
     end
 
     it "inactive? returns false when active is true" do
-      resource_category = ResourceCategory.new(active:true)
-      expect(resource_category.inactive?).to be_falsy
+      build_cat.active = true
+      expect(build_cat.inactive?).to be_falsy
     end
 
     it "active? returns true when category is active" do
-      resource_category = ResourceCategory.new(active:true)
-      expect(resource_category.active?).to be_truthy
+      inactive.activate
+      expect(inactive.active?).to be_truthy
     end
 
     it "active? returns false when category is inactive" do
-      resource_category = ResourceCategory.new(active:false)
-      expect(resource_category.active?).to be_falsy
+      expect(inactive.active?).to be_falsy
     end
 
     it "to_s returns its name when called" do
-      fake_name = "Fake Category"
-      resource_category = ResourceCategory.new(name:fake_name)
-      expect(resource_category.to_s).to eq(fake_name)
+      fake_name = build_cat.name
+      expect(build_cat.to_s).to eq(fake_name)
     end
 
     # it ":active scope returns all active Resource categories", skip:true do
