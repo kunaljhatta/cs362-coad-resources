fcf27fd5a0abb9388175f9a28800c665a4d6fc45
KunalJhatta <Kunaljhatta22@gmail.com>
2022-02-10 19:24:11 -0800

    Refactor region tests to use factory References #35

--- spec/factories/regions.rb
+++ spec/factories/regions.rb
@@ -1,12 +1,10 @@
 FactoryBot.define do
   factory :region do
-
     transient do
       unique_name { false }
     end
-
     # name {'Fake Region'}
     sequence(:name) { |n| "Fake Region#{" #{n}" if unique_name}" }
-    
   end
+
 end
