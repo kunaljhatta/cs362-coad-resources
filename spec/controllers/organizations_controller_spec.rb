316d7224d240924e399708c7dd9d0a6babb0f15f
KunalJhatta <Kunaljhatta22@gmail.com>
2022-02-25 12:36:48 -0800

    Write organizations  controller tests for public methods References #44

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe OrganizationsController, type: :controller do
-
+    describe "a non logged in person" do 
+        it "redirects to the sign in screen" do 
+            get :index
+            expect(response). to redirect_to(new_user_session_path)
+            get :new
+            expect(response). to redirect_to(new_user_session_path)
+            post :create
+            expect(response). to redirect_to(new_user_session_path)
+            get :edit, params: {id: "FAKE"}
+            expect(response). to redirect_to(new_user_session_path)
+            put :update, params: {id: "FAKE"}
+            expect(response). to redirect_to(new_user_session_path)
+            patch :update, params: {id: "FAKE"}
+            expect(response). to redirect_to(new_user_session_path)
+            get :show, params: {id: "FAKE"}
+            expect(response). to redirect_to(new_user_session_path)
+            post :approve, params: {id: "FAKE"}
+            expect(response). to redirect_to(new_user_session_path)
+            post :reject, params: {id: "FAKE"}
+            expect(response). to redirect_to(new_user_session_path)
+        end 
+    end 
 end
