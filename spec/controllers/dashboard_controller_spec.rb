af4cabec5373c8ab79a74f61e3365da389ce2b17
Paul Koos <paully_k5298@yahoo.com>
2022-02-28 22:28:37 -0800

    add describe blocks for dashboard controller methods.
    References #52.

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe DashboardController, type: :controller do
+  describe "index" do
+
+  end
+
+  describe "status_options" do
+
+  end
+
+  describe "tickets" do
+
+  end
 
 end
