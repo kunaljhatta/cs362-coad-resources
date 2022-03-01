5f72f6d1810f3556bf4cc1cadad8b19722817019
Erin Szabo <eszabo@cocc.edu>
2022-03-01 15:32:21 -0800

    Describe a non logged in person.
    Reference #48

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -2,8 +2,22 @@ require 'rails_helper'
 
 RSpec.describe TicketsController, type: :controller do
 
-    describe "a non logged in person"
-    describe "a citizen user"
-    describe "an organization user"
-    
+    describe "a non logged in person" do
+        it "redirects to the log-in screen" do
+            get :show, params: {id: 'fake'}
+            expect(response).to redirect_to(dashboard_path)
+            post :capture, params: {id: 'fake'}
+            expect(response).to redirect_to(dashboard_path)
+            post :release, params: {id: 'fake'}
+            expect(response).to redirect_to(dashboard_path)
+            patch :close, params: {id: 'fake'}
+            expect(response).to redirect_to(dashboard_path)
+            delete :destroy, params: {id: 'fake'}
+            expect(response).to redirect_to(dashboard_path)
+        end
+    end
+
+    describe "an unapproved organization user"
+    describe "an unapproved organization user"
+
 end
