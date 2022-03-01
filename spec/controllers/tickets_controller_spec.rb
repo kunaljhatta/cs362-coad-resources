a08439789f623b5fef0ec46a9b84632f5db08833
Erin Szabo <eszabo@cocc.edu>
2022-03-01 15:51:10 -0800

    Reformat and plan next tests.
    Reference #48

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -2,22 +2,27 @@ require 'rails_helper'
 
 RSpec.describe TicketsController, type: :controller do
 
-    describe "a non logged in person" do
-        it "redirects to the log-in screen" do
-            get :show, params: {id: 'fake'}
-            expect(response).to redirect_to(dashboard_path)
-            post :capture, params: {id: 'fake'}
-            expect(response).to redirect_to(dashboard_path)
-            post :release, params: {id: 'fake'}
-            expect(response).to redirect_to(dashboard_path)
-            patch :close, params: {id: 'fake'}
-            expect(response).to redirect_to(dashboard_path)
-            delete :destroy, params: {id: 'fake'}
-            expect(response).to redirect_to(dashboard_path)
-        end
+  describe "a non logged in person" do
+    it "redirects to the log-in screen" do
+      get :show, params: {id: 'fake'}
+        expect(response).to redirect_to(dashboard_path)
+        post :capture, params: {id: 'fake'}
+        expect(response).to redirect_to(dashboard_path)
+        post :release, params: {id: 'fake'}
+        expect(response).to redirect_to(dashboard_path)
+        patch :close, params: {id: 'fake'}
+        expect(response).to redirect_to(dashboard_path)
+        delete :destroy, params: {id: 'fake'}
+        expect(response).to redirect_to(dashboard_path)
+      end
     end
 
-    describe "an unapproved organization user"
-    describe "an unapproved organization user"
+    describe "an unapproved organization user" do
+      it "redirects to the dashboard" 
+    end
+    
+    describe "an approved organization user" do
+      it "redirects to the dashboard"
+    end
 
 end
