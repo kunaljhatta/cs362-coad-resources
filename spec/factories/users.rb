f3326cd7be68b85b1d7bbebcec9c3f6207feef5b
KunalJhatta <Kunaljhatta22@gmail.com>
2022-03-03 14:29:33 -0800

    fixed code duplication in users factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,15 +2,11 @@ FactoryBot.define do
   factory :user do
     email { "factory_user@fake.fake" }
     password { "factory_password" }
-  
-    trait :organization_user do
-      role {"organization"}
-      organization
-    end
   end
-  
+
   trait :organization_user do
+    role {"organization"}
     organization
-    role { 'organization' }
   end
 end
+
