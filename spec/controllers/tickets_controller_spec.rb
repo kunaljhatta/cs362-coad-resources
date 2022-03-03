af91238743c75d1a67a7ad18e9b22cd58d28dd65
Erin Szabo <eszabo@cocc.edu>
2022-03-03 15:52:21 -0800

    Complete tickets controller tests.
    Reference #48

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -19,33 +19,32 @@ RSpec.describe TicketsController, type: :controller do
 
   describe "an unapproved organization user" do
     it "redirects to the dashboard" do
-      unapproved_org = create(:user, :organization_user)
-      # expect(unapproved_org.status).to eq("submitted")
-      # sign_in(unapproved_org)
-      # log in user as unapproved organization
-      # confirm this
-      # check that this user is redirected 
-      # if they try to do something they shouldn't be able to do
+      unapproved_org_user = create(:user, :organization_user)
+      expect(unapproved_org_user.organization.status).to eq("submitted")
+      sign_in(unapproved_org_user)
 
-      # show
-      # capture
-      # release
-      # close
-      # destroy
+      get :show, params: {id: 'fake'}
+      expect(response).to redirect_to("http://test.host/users/sign_in")
+      post :capture, params: {id: 'fake'}
+      expect(response).to redirect_to(dashboard_path)
+      post :release, params: {id: 'fake'}
+      expect(response).to redirect_to(dashboard_path)
+      patch :close, params: {id: 'fake'}
+      expect(response).to redirect_to(dashboard_path)
+      delete :destroy, params: {id: 'fake'}
+      expect(response).to redirect_to(dashboard_path)
     end
   end
   
   describe "an approved organization user" do
     it "redirects to the dashboard" do
-      # log in as an approved organization
-      # destroy
+      unapproved_org_user = create(:user, :organization_user)
+      unapproved_org_user.organization.approve
+      expect(unapproved_org_user.organization.status).to eq("approved")
+      sign_in(unapproved_org_user)
 
-
-    #   approved_user = create(:organization)
-    #   expect(approved_org.status).to eq("approved")
-    #   sign_in(approved_org)
-    #   delete :destroy, params: {id: 'fake'}
-    #   expect(response).to redirect_to(dashboard_path)
+      delete :destroy, params: {id: 'fake'}
+      expect(response).to redirect_to("http://test.host/users/sign_in")
     end
   end
 
