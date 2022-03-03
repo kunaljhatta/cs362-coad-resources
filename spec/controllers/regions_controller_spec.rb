1d4ebafb8ca3ed144af03b4cc53e4394c2ec68f3
KunalJhatta <Kunaljhatta22@gmail.com>
2022-03-03 13:46:38 -0800

    add test for logged in organization user References #44

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -21,4 +21,28 @@ RSpec.describe RegionsController, type: :controller do
             expect(response).to redirect_to(new_user_session_path)
         end
     end  
+    
+    describe "logged in organization user" do
+        it "redirects to dashboard" do
+            organization_user = build(:user, :organization_user)
+            organization_user.confirm
+            sign_in(organization_user)
+            get :index
+            expect(response).to redirect_to(dashboard_url)
+            get :show, params: {id: 'fake'}
+            expect(response).to redirect_to(dashboard_url)
+            get :new
+            expect(response).to redirect_to(dashboard_url)
+            post :create
+            expect(response).to redirect_to(dashboard_url)
+            get :edit, params: {id: 'fake'}
+            expect(response).to redirect_to(dashboard_url)
+            delete :destroy, params: {id: 'FAKE'}
+            expect(response).to redirect_to(dashboard_url)
+            patch :update, params: {id: 'FAKE'}
+            expect(response).to redirect_to(dashboard_url)
+            put :update, params: {id: 'FAKE'}
+            expect(response).to redirect_to(dashboard_url)
+        end
+    end
 end
--- spec/factories/regions.rb
+++ spec/factories/regions.rb
@@ -3,7 +3,6 @@ FactoryBot.define do
     transient do
       unique_name { false }
     end
-    # name {'Fake Region'}
     sequence(:name) { |n| "Fake Region#{" #{n}" if unique_name}" }
   end
 
