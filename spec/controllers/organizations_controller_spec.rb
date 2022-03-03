bb1983f709244878065ad862a532ea42dc868b5c
KunalJhatta <Kunaljhatta22@gmail.com>
2022-03-03 14:21:49 -0800

    add test for organization user that redirects to the dashboard page References #43

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -24,30 +24,29 @@ RSpec.describe OrganizationsController, type: :controller do
         end 
     end   
 
-    describe  "a organization user" do 
-        it "redirects to the sign in screen" do
-            organization_user = create(:user)
-            organization_user.confirm
-            # organization_user.organization.approve
-            sign_in(organization_user)
-            # get :index
-            # expect(response). to redirect_to(new_user_session_path)
-            # get :new
-            # expect(response). to redirect_to(new_user_session_path)
-            # post :create
-            # expect(response). to redirect_to(new_user_session_path)
-            # get :edit, params: {id: "FAKE"}
-            # expect(response). to redirect_to(new_user_session_path)
-            # put :update, params: {id: "FAKE"}
-            # expect(response). to redirect_to(new_user_session_path)
-            # patch :update, params: {id: "FAKE"}
-            # expect(response). to redirect_to(new_user_session_path)
-            # get :show, params: {id: "FAKE"}
-            # expect(response). to redirect_to(new_user_session_path)
-            # post :approve, params: {id: "FAKE"}
-            # expect(response). to redirect_to(new_user_session_path)
-            # post :reject, params: {id: "FAKE"}
-            # expect(response). to redirect_to(new_user_session_path)
+    describe "organization user" do
+        it "redirects to the dashboard page" do
+            org_user = create(:user, :organization_user)
+            org_user.confirm
+            sign_in(org_user)
+            get :index
+            expect(response).to be_successful
+            get :new
+            expect(response).to redirect_to(dashboard_path)
+            post :create
+            expect(response).to redirect_to(dashboard_path)
+            get :edit, params:{id: "FAKE"}
+            expect(response).to redirect_to(dashboard_path)
+            patch :update, params:{id: "FAKE"}
+            expect(response).to redirect_to(dashboard_path)
+            put :update, params:{id: "FAKE"}
+            expect(response).to redirect_to(dashboard_path)
+            get :show, params:{id: "FAKE"}
+            expect(response).to redirect_to(dashboard_path)
+            post :approve, params:{id: "FAKE"}
+            expect(response).to redirect_to(dashboard_path)
+            post :reject, params:{id: "FAKE"}
+            expect(response).to redirect_to(dashboard_path)
         end
-    end 
+    end
 end
