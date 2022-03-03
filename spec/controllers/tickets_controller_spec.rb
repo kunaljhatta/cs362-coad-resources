4eed3cf7e006db68108a2b860d2ffba370b5efff
Erin Szabo <eszabo@cocc.edu>
2022-03-03 14:41:11 -0800

    Setup what to test and how.
    Reference #48

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -18,17 +18,35 @@ RSpec.describe TicketsController, type: :controller do
   end
 
   describe "an unapproved organization user" do
-    it "redirects to the dashboard" 
+    it "redirects to the dashboard" do
+      unapproved_org = create(:organization)
+      expect(unapproved_org.status).to eq("submitted")
+      # sign_in(unapproved_org)
+      # log in user as unapproved organization
+      # confirm this
+      # check that this user is redirected 
+      # if they try to do something they shouldn't be able to do
+
+      # show
+      # capture
+      # release
+      # close
+      # destroy
+    end
   end
   
   describe "an approved organization user" do
-    # it "redirects to the dashboard" do
-    #   approved_org_user = create(:user, organization:create(organization))
-    #   approved_org_user.organization.approve
-    #   sign_in(approved_org_user)
+    it "redirects to the dashboard" do
+      # log in as an approved organization
+      # destroy
+
+
+    #   approved_user = create(:organization)
+    #   expect(approved_org.status).to eq("approved")
+    #   sign_in(approved_org)
     #   delete :destroy, params: {id: 'fake'}
     #   expect(response).to redirect_to(dashboard_path)
-    # end
+    end
   end
 
 end
