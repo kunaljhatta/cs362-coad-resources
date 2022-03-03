686e7d8558af3ba45926d4a14709bbc3dcc2aa77
Erin Szabo <eszabo@cocc.edu>
2022-03-03 15:02:36 -0800

    Fix Users factory to be able to use trait.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,11 +2,10 @@ FactoryBot.define do
   factory :user do
     email { "factory_user@fake.fake" }
     password { "factory_password" }
-  end
 
-  trait :organization_user do
-    role {"organization"}
-    organization
+    trait :organization_user do
+      role {"organization"}
+      organization
+    end
   end
 end
-
