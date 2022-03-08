14221e654e097391d68842b7bdeddd698a5fd80b
Erin Szabo <eszabo@cocc.edu>
2022-03-08 11:17:07 -0800

    Plan how to log in as admin.
    Reference #54

--- spec/features/tickets/admin_deletes_ticket_spec.rb
+++ spec/features/tickets/admin_deletes_ticket_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe 'Deleting a Ticket', type: :feature do
+  scenario "admin deletes a ticket" do
+    # given: logged in as admin and on a page of a ticket
+    #-- admin = create(:user)
+    #-- log_in_as(admin)
+    #-- visit(tickets_page)
 
+    # when: admin clicks 'delete'
+    # then: ticket should be deleted and admin is redirected to
+    #       the dashboard
+  end
 end
