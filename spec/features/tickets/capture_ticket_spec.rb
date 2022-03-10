0cb562c3d5339a7ee533aa7d2429ec41a6e54755
Erin Szabo <eszabo@cocc.edu>
2022-03-09 17:11:36 -0800

    Setup capturing a ticket test.
    Reference #55

--- spec/features/tickets/capture_ticket_spec.rb
+++ spec/features/tickets/capture_ticket_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe 'Capturing a ticket', type: :feature do
+  scenario "organization captures a ticket" do
+    # given: logged in as an organization user and viewing a ticket
+    ticket = create(:ticket)
+    user = create(:user, :organization_user) 
+    user.organization.approve
+    user.confirm
+    user.save!
+    log_in_as(user)
+    # visit "/dashboard"
+    visit "/tickets/1"
+    # expect(page).to have_text "Capture"
 
+    # when: when capture button is clicked
+    # then: the ticket is captured and org user is redirected to dashboard
+  end
 end
