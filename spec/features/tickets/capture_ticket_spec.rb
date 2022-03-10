8151e956f62129a3619c7f7fb34655a5b6205943
Erin Szabo <eszabo@cocc.edu>
2022-03-10 10:41:28 -0800

    Setup the givens, org user and ticket.
    Reference #55

--- spec/features/tickets/capture_ticket_spec.rb
+++ spec/features/tickets/capture_ticket_spec.rb
@@ -4,16 +4,25 @@ RSpec.describe 'Capturing a ticket', type: :feature do
   scenario "organization captures a ticket" do
     # given: logged in as an organization user and viewing a ticket
     ticket = create(:ticket)
+
+    expect(ticket.captured?).to be_falsy
     user = create(:user, :organization_user) 
     user.organization.approve
+    user.organization.save!
     user.confirm
     user.save!
     log_in_as(user)
-    # visit "/dashboard"
-    visit "/tickets/1"
-    # expect(page).to have_text "Capture"
+    visit "/tickets/#{ticket.id}" 
+    click_on "Release"
+    visit "/tickets/#{ticket.id}" 
 
     # when: when capture button is clicked
-    # then: the ticket is captured and org user is redirected to dashboard
+    click_on "Capture"
+    #expect(page).to have_text "Captue"
+
+    # then: the ticket is captured and org user is redirected to dashboard/Tickets page
+    #expect(ticket.captured?).to be_truthy
+    # expect(page).to have_text "Tickes"
+
   end
 end
