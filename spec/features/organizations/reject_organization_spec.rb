7737d5fd5ec0e2ef8041c6a4d5a6c6af1efd82bc
KunalJhatta <Kunaljhatta22@gmail.com>
2022-03-12 15:26:00 -0800

    admin rejects a submitted organization References #63

--- spec/features/organizations/reject_organization_spec.rb
+++ spec/features/organizations/reject_organization_spec.rb
@@ -2,4 +2,14 @@ require 'rails_helper'
 
 RSpec.describe 'Rejecting an organization', type: :feature do
 
+    let(:admin) {create(:user, :admin)}
+    let(:organization_submitted) {create(:organization, :set_submitted)}
+
+    scenario "admin rejects a submitted organization" do
+        admin.confirm
+        log_in_as(admin)
+        visit organization_path(organization_submitted)
+        click_on "Reject"
+        expect(page).to have_text('has been rejected.')
+    end
 end
