dd23a12d7b8f6abd97ce53f628c79c595c47d6d0
Erin Szabo <eszabo@cocc.edu>
2022-03-13 19:01:22 -0700

    Setup and work on the givens.
    Reference #58

--- spec/features/tickets/release_ticket_spec.rb
+++ spec/features/tickets/release_ticket_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe 'Releasing a ticket by an', type: :feature do
+  scenario "organization releases a ticket" do
+    # given: logged in as an organization user and viewing a ticket
+    ticket = create(:ticket)
+    ticket.organization = nil
+    expect(ticket.captured?).to be_falsy
+    user = create(:user, :organization_user) 
+    user.organization.approve
+    user.organization.save!
+    user.confirm
+    user.save!
 
+    ticket.organization = user.organization
+    expect(ticket.captured?).to be_truthy
+
+    log_in_as(user)
+    visit "/tickets/#{ticket.id}" 
+
+
+    # when: when release button is clicked
+    click_on "Release"
+
+    # then: the ticket is released and org user is redirected to dashboard/Tickets page
+    #expect(ticket.captured?).to be_falsy
+    expect(page).to have_text "Tickets"
+
+  end
 end
