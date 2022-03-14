fa2213872042395d1cf43b6876f6dc976e0aac45
Paul Koos <paully_k5298@yahoo.com>
2022-03-13 23:07:28 -0700

    add unit tests for application_helper.
    References #73.

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,4 +2,18 @@ require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
 
+  describe "methods" do
+    
+    base_title = 'Disaster Resource Network'
+    page_title = 'Fake Page'
+    param_title = "#{page_title} | #{base_title}"
+
+    it "full_title prints out the base title with no parameters" do
+      expect(full_title).to eq(base_title)
+    end
+
+    it "full_title concatenates parameter to the front of returned string" do
+      expect(full_title(page_title)).to eq(param_title)
+    end
+  end
 end
