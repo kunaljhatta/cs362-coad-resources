3a2111ab5767a512a2a74638c52b82500a1677bf
Paul Koos <paully_k5298@yahoo.com>
2022-03-14 00:36:07 -0700

    add help tests for dashboard helper.

--- spec/helpers/dashboard_helper_spec.rb
+++ spec/helpers/dashboard_helper_spec.rb
@@ -12,4 +12,20 @@ require 'rails_helper'
 # end
 RSpec.describe DashboardHelper, type: :helper do
 
+  describe "methods" do
+    let(:org) { build_stubbed(:organization) }
+    let(:admin) { build_stubbed(:user, :admin) }
+    let(:user) { build_stubbed(:user)}
+
+
+    it "prints out a different string based on the permissions level of the user" do
+      expect(dashboard_for(admin)).to eq('admin_dashboard')
+      org_user = org.users.build
+      expect(dashboard_for(org_user)).to eq('organization_submitted_dashboard')
+      org.approve
+      org_approved = org.users.build
+      expect(dashboard_for(org_approved)).to eq('organization_approved_dashboard')
+      expect(dashboard_for(user)).to eq('create_application_dashboard')
+    end
+  end
 end
