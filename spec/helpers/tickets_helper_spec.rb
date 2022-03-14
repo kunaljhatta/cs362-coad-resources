a70eb15746236302ab95c47da45b1e82f30f9464
Paul Koos <paully_k5298@yahoo.com>
2022-03-13 23:58:19 -0700

    add helper test for ticket helper, used to correct the format of phone numbers.
    References 76.

--- spec/helpers/tickets_helper_spec.rb
+++ spec/helpers/tickets_helper_spec.rb
@@ -1,15 +1,17 @@
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the TIcketsHelper. For example:
-#
-# describe TIcketsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe TicketsHelper, type: :helper do
 
+  describe "methods" do
+    ph_number = "5415551212"
+    ph_number2 = "+1(541)555-1212"
+    ph_number3 = "541-555-1212"
+    ph_number4 = "(541-555-1212)"
+    it "changes poorly formatted numbers into the US format" do
+      expect(format_phone_number(ph_number)).to eq("+15415551212")
+      expect(format_phone_number(ph_number2)).to eq("+15415551212")
+      expect(format_phone_number(ph_number3)).to eq("+15415551212")
+      expect(format_phone_number(ph_number4)).to eq("+15415551212")
+    end
+  end
 end
